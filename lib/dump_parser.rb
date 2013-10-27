e806be53210fb08ce60c0d09158811d3fb92db5e
Andrew Grimm <andrew.j.grimm@gmail.com>
2013-10-27 17:00:54 +1100

    Initial commit

---
+++ lib/dump_parser.rb
@@ -0,0 +1,59 @@
+require "json"
+
+class DumpParser
+  INPUT_FILENAME = "test/data/quick_intersection_20131027.json"
+
+  def self.run
+    text = File.read(INPUT_FILENAME)
+    data = JSON.parse(text)
+    dump_parser = new(data)
+    puts dump_parser.word_frequency_analysis
+  end
+
+  def initialize(data)
+    @data = data
+
+    @page_titles = create_page_titles
+    @words = @page_titles.flat_map(&:words)
+
+    @word_frequency = determine_word_frequency
+  end
+
+  def create_page_titles
+    pages = @data.fetch("pages")
+    pages.map(&method(:create_page_title))
+  end
+
+  def create_page_title(page)
+    page_title = page.fetch("page_title")
+    PageTitle.new(page_title)
+  end
+
+  def determine_word_frequency
+    words_grouped_by_word = @words.group_by {|word| word}
+    words_grouped_by_word.each_with_object({}) do |(word, words), result|
+      result[word] = words.length
+    end
+  end
+
+  def word_frequency_analysis
+    title_row = ["Word", "Frequency"]
+    body_rows = @word_frequency.sort_by {|_word, frequency| -frequency}
+    rows = [title_row] + body_rows
+    rows.map{|row| row.join("\t")}.join("\n")
+  end
+end
+
+class PageTitle
+  attr_reader :words
+
+  def initialize(page_title)
+    @page_title = page_title
+
+    @words = determine_words
+  end
+
+  def determine_words
+    @page_title.split("_")
+  end
+end
