f763b89ad71871edc842d3765006a1f95391133a
Andrew Grimm <andrew.j.grimm@gmail.com>
2013-10-27 19:07:09 +1100

    Squash rubocop warnings

--- lib/dump_parser.rb
+++ lib/dump_parser.rb
@@ -1,5 +1,6 @@
-require "json"
+require 'json'
 
+# Parses JSON dumps of titles in a category
 class DumpParser
   INPUT_FILENAME = "test/data/quick_intersection_20131027.json"
 
@@ -20,30 +21,33 @@ class DumpParser
   end
 
   def create_page_titles
-    pages = @data.fetch("pages")
+    pages = @data.fetch('pages')
     pages.map(&method(:create_page_title))
   end
 
   def create_page_title(page)
-    page_title = page.fetch("page_title")
+    page_title = page.fetch('page_title')
     PageTitle.new(page_title)
   end
 
   def determine_word_frequency
-    words_grouped_by_word = @words.group_by {|word| word}
+    words_grouped_by_word = @words.group_by { |word| word }
     words_grouped_by_word.each_with_object({}) do |(word, words), result|
       result[word] = words.length
     end
   end
 
   def word_frequency_analysis
-    title_row = ["Word", "Frequency"]
-    body_rows = @word_frequency.sort_by {|_word, frequency| -frequency}
+    title_row = %w{Word Frequency}
+    body_rows = @word_frequency.sort_by do |word, frequency|
+      [-frequency, word]
+    end
     rows = [title_row] + body_rows
-    rows.map{|row| row.join("\t")}.join("\n")
+    rows.map { |row| row.join("\t") }.join("\n")
   end
 end
 
+# Converts page titles into words
 class PageTitle
   attr_reader :words
 
@@ -54,6 +58,6 @@ class PageTitle
   end
 
   def determine_words
-    @page_title.split("_")
+    @page_title.split('_')
   end
 end
